38e85a68dc97a2f6d9eaab9fd8f30a4062d4a0c0
Azeroual Jordan <Jordan@MacBook-Pro-de-Azeroual.local>
2016-09-16 18:40:18 +0200

    16/09

---
+++ framework-standard-edition/src/AppBundle/Entity/Offer.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Offer
+ */
+class Offer
+{
+    /**
+     * @var int
+     */
+    private $id;
+    
+    /**
+     * @var int
+     */
+    private $name;
+    
+    /**
+     * @var int
+     */
+    private $creationDate;
+    
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Offer
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set creationDate
+     *
+     * @param \DateTime $creationDate
+     *
+     * @return Offer
+     */
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get creationDate
+     *
+     * @return \DateTime
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+}
